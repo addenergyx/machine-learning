62f3e7ecb76c6a3827530ecf1e5899f5e78fd60d
da12 <da12@sanger.ac.uk>
2018-03-20 16:10:18 +0000

    Change to regex as some paths have '-'

--- copy_alleles_frequency.pl
+++ copy_alleles_frequency.pl
@@ -4,8 +4,9 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-# Example of path to file 
+# Examples of path to file 
 # /warehouse/team229_wh01/lims2_managed_miseq_data/Miseq_026/S143_expSNCA_E35K_4/CRISPResso_on_143_S143_L001_R1_001_143_S143_L001_R2_001/Alleles_frequency_table.txt
+# /warehouse/team229_wh01/lims2_managed_miseq_data/Miseq_001_S10_expKMT2C_B_CRISPResso_on_Homo-sapiens_S10_L001_R2_001_Alleles_frequency_table.txt
 
 GetOptions(
     'path=s' => \my $path,
@@ -24,7 +25,7 @@ chomp @files; # Removes new line from each filename
 foreach my $file (@files) {
     $file =~ s/\.\///;
     (my $name = $file) =~ s/\//_/g;
-    $name =~ s/_CRISPResso[A-Za-z0-9_]+\d//g;
+    $name =~ s/_CRISPResso[A-Za-z0-9_-]+\d//g;
     my $dest_path = "./alleles_frequency/$name";
     # when using system commands it's best to use the absolute path of the command and not just cp as cp could be simply aliased to something else
     system("/bin/cp $file $dest_path");
