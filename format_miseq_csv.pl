0ffbcf0edc5d6c640abb2376d6c21d9462daae40
da12 <da12@sanger.ac.uk>
2018-04-12 15:31:24 +0000

    Clean up

--- format_miseq_csv.pl
+++ format_miseq_csv.pl
@@ -6,7 +6,6 @@ use warnings;
 use Text::CSV_XS;
 use Text::CSV::Slurp;
 use Getopt::Long;
-use Data::Dumper;
 
 GetOptions(
 	'tabs'			=>	\my $tabs,
@@ -22,29 +21,24 @@ my @dataset;
 if (scalar @files == 0){@files = @ARGV}
 
 foreach my $file (@files) {
-	#shift @files;
 	open (my $fh, '<', $file) or die "'$file':$!\n";
 	my $headers = $csv->getline($fh);
 	$csv->column_names(@{$headers});
 	while (my $row = $csv->getline_hr($fh)) {
 		delete $row->{'Reference_Sequence'};
 		push (@dataset, $row);
-#$DB::single=1;
 		print "\nRow " . scalar @dataset . " in file $file has been modified\n" ;
 	}
 	close $fh;
 
 	open (my $out, '>', $file) or die "'$file'";
-$DB::single=1;
 	my $element = 'Reference_Sequence';
 	@{$headers} = grep {$_ ne $element} @{$headers};
-	#splice @{$headers},1,1;
 	my $slurp = Text::CSV::Slurp->create(input=>\@dataset, field_order => \@{$headers});
 	print $out $slurp;
-
 	close $out;
 	print "Closed $file";
-	undef (@dataset);
-	# undef(@array) frees all the memory associated with the array except the bare minimum
+	# undef(@array) frees all the memory associated with the array
+	undef (@dataset); 
 }
 
