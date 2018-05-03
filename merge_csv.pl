f42eb422388e520c583e64effb49ddf729c52820
da12 <da12@sanger.ac.uk>
2018-05-03 15:58:40 +0000

    Script to merge csv files

---
+++ merge_csv.pl
@@ -0,0 +1,33 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+use Text::CSV_XS;
+use File::Copy qw(copy);
+
+my @file;
+my $headers;
+my $tmp;
+
+foreach my $file (@ARGV) {
+	
+	# Add new line to end of file
+	#`echo >> $file`;
+
+	open my $data, '<', $file or die $!;
+	
+	my @lines = <$data>;
+	
+	$headers = shift @lines;
+	push (@file, @lines);
+	close $data;
+	
+	# Remove new line after
+	#`perl -i -ne 'print unless eof' $file`
+}
+
+unshift (@file, $headers);
+open my $out, '>', 'Merged.csv' or die $!;
+print $out @file;
+close $out;
