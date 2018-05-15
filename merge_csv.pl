e4eec54d80823392fc8f0cbce0ac535baa08bf6b
da12 <da12@sanger.ac.uk>
2018-05-15 13:27:58 +0000

    Fixed error where lines in output were merging

--- merge_csv.pl
+++ merge_csv.pl
@@ -1,9 +1,9 @@
 #! /usr/bin/perl
 
-# For classifcation must merge data before running preprocessing script, this is because each file would contain headers of different lengths due to varying sequence sizes. The classifaction script handles this and uses the longest sequence size as the header.
+# For classifcation must merge data before running preprocessing script, this is because each file would contain headers of different lengths due to varying sequence sizes. 
+# The classifaction script handles this and uses the longest sequence size as the header.
 # For regression run script on the data first then merge the files
  
-
 use strict;
 use warnings;
 
@@ -13,7 +13,8 @@ my $headers;
 foreach my $file (@ARGV) {
 	
 	# Add new line to end of file
-	#`echo >> $file`;
+	# This is so the last result in one file doesn't merge with the beginning of another  
+	`echo >> $file`;
 
 	open my $data, '<', $file or die $!;
 	
@@ -22,12 +23,13 @@ foreach my $file (@ARGV) {
 	$headers = shift @lines;
 	push (@file, @lines);
 	close $data;
-	
+
 	# Remove new line after
 	#`perl -i -ne 'print unless eof' $file`
+	print "Stored file $file\n"; 	
 }
-
 unshift (@file, $headers);
-open my $out, '>', 'Merged.csv' or die $!;
+open my $out, '>', 'multivariate_20_merged_data.csv' or die $!;
+print "Copying files to output\n";
 print $out @file;
 close $out;
