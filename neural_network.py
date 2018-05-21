7a4d966ccb41a759938a7af0d63c409bfa7beb13
da12 <da12@sanger.ac.uk>
2018-05-21 14:18:04 +0000

    Can make predictions on multiple results from a file

--- neural_network.py
+++ neural_network.py
@@ -18,7 +18,7 @@ config.add_argument('-t','--tensorboard', nargs='?', const='{0}/machine-learning
 config.add_argument('-s','--save', nargs='?', const=home + "/machine-learning/snapshots/%s_trained_model.h5" % date, help="Save model to disk")
 config.add_argument('-v','--verbose', action="store_true", help="Verbosity mode")
 config.add_argument('-p','--predict', nargs='+',
-                    help="Sample data for model to make a prediction on. False = 0, True = 1. Must be in order: NHEJ,UNMODIFIED,HDR,n_mutated,a_count,c_count,t_count,g_count,gc_content,tga_count,ttt_count,minimum_free_energy_prediction,pam_count,length,frameshift,#Reads,%%Reads. For example: 0 1 0 0 68 77 39 94 68 2 1 -106.400001525879 26 278 0 1684 34.9885726158")
+                    help="Can parse a single observation or file containing multiple observations to make predictions on. False = 0, True = 1. Must be in order: NHEJ,UNMODIFIED,HDR,n_mutated,a_count,c_count,t_count,g_count,gc_content,tga_count,ttt_count,minimum_free_energy_prediction,pam_count,length,frameshift,#Reads,%%Reads. For example: 0,1,0,0,68,77,39,94,68,2,1,-106.400001525879,26,278,0,1684,34.988572615")
 config.add_argument('-l','--load', const=home + '/machine-learning/snapshots/03-05-18_best_model.h5', help="Path to saved model", nargs='?')
 config.add_argument('-b', '--batch', nargs='?', const=home + '/machine-learning/smallsamplefiles', 
                     help="Path to directory containing multiple files with data in the correct format. Default: ~/machine-learning/smallsamplefiles/")
@@ -55,10 +55,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from time import strftime
 from turicreate import SFrame
-#import csv
-#from csvvalidator import *
 
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+
+#Feature scaling -1 to +1 because there will be alot of parallel computations
+#can use standardisation or normalisation
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+    
 def build_data(sample):
+    
     #Importing dataset
     dataset = pd.read_csv(sample, error_bad_lines=False)
 
@@ -70,16 +78,28 @@ def build_data(sample):
     
     #Length of input, will be used when building model
     input_dim = len(headers) - (3 if multivariate else 2) 
-            
+        
+    #Encoding values, assigning each catagory a number
+        
+    #Encoding NHEJ
+    dataset['NHEJ'] = labelencoder.fit_transform(dataset['NHEJ'])
+    
+    #Encoding UNMODIFIED
+    dataset['UNMODIFIED'] = labelencoder.fit_transform(dataset['UNMODIFIED'])
+    
+    #Encoding frameshift
+    dataset['frameshift'] = labelencoder.fit_transform(dataset['frameshift'])
+    
+    #Encoding HDR
+    dataset['HDR'] = labelencoder.fit_transform(dataset['HDR'])
+    
     #Input layer
     X = dataset.iloc[:, 0:input_dim + 1].values
             
     #Output layer
     Y = dataset.iloc[:, input_dim + 1:20].values if multivariate else dataset.iloc[: , (input_dim + 1):(input_dim + 2)].values 
-            
-    #Encoding categorical data
-    from sklearn.preprocessing import LabelEncoder
-    
+
+    '''
     #Encoding values, assigning each catagory a number
         
     #Encoding NHEJ
@@ -94,16 +114,13 @@ def build_data(sample):
     
     #Encoding HDR
     X[:,3] = labelencoder.fit_transform(X[:,3])
+    '''
     
     #Spliting dataset into training and test set
     from sklearn.model_selection import train_test_split
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
         
-    #Feature scaling -1 to +1 because there will be alot of parallel computations
-    #can use standardisation or normalisation
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    
+
     #Dropping aligned sequence here instead of at the beginning so it can later be
     #appened to prediction table
     # input_dim or input_dim + 1
@@ -122,10 +139,10 @@ def build_data(sample):
     
     """Use alot of objects so in the future it would be easy to implement a different algorithm
     Would only need to change this library"""
-    return X_test, Y_test, input_dim, X_train, Y_train, test_aligned_sequence, headers, sc
+    return X_test, Y_test, input_dim, X_train, Y_train, test_aligned_sequence, headers
 
 #Building complete prediction table
-def build_csv(test_aligned_sequence, sc):
+def build_csv(test_aligned_sequence):
     inverse_x = sc.inverse_transform(X_test)
     test_aligned_sequence = np.reshape(test_aligned_sequence, (-1,1))
     pred_set = np.concatenate((test_aligned_sequence,inverse_x,Y_test,y_pred),axis=1)
@@ -169,15 +186,15 @@ def root_mean_squared_error(y_true, y_pred):
 
 def visualisation(y_pred, Y_test, y_difference, filename='vis.html'):
     #print(history.history.keys())
-    
+
     #Big datasets are not represented by the graph very well so will only show first
     # 2000 results
-    
+
     if len(y_difference) > 2000:
         y_pred = y_pred[0:2000]
         Y_test = Y_test[0:2000]
         y_difference = y_difference[0:2000]
-    
+
     if saved_model is None:
         plt.title('Model accuracy (RMSE)')
         plt.plot(history.history['root_mean_squared_error'])
@@ -185,7 +202,7 @@ def visualisation(y_pred, Y_test, y_difference, filename='vis.html'):
         plt.ylabel('Accuracy')
         plt.xlabel('Epoch')
         plt.show()
-    
+
     #plt.plot(history.history['loss'])
     #plt.show()
 
@@ -204,17 +221,17 @@ def visualisation(y_pred, Y_test, y_difference, filename='vis.html'):
     plt.title('Difference between predicted results and actual results')
     plt.plot(y_difference)
     plt.show()
-    
+
     from bokeh.plotting import figure, output_file, show
     # output to static HTML file
     output_file(filename)
     # create a new plot with a title and axis labels
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    plot = figure(title="Difference between predicted results and actual results", x_axis_label='x', y_axis_label='y')
     # add a line renderer with legend and line thickness
-    p.line(range(len(y_difference)), np.ravel(y_difference), legend="Temp.", line_width=2)
+    plot.line(range(len(y_difference)), np.ravel(y_difference), legend="Difference", line_width=2)
     # show the results
-    show(p)
-    
+    show(plot)
+
     return
 
 if saved_model is not None:
@@ -226,29 +243,29 @@ if saved_model is not None:
     X_test, Y_test = build_data(sample)[0:2]
     score = model.evaluate(X_test, Y_test, verbose=verbose)
     print("%s: %.2f" % (model.metrics_names[1], score[1]))
-    
+
     #predict
     '''Currently this part is written twice (once for each model). Should look into having
     this outside the if/else so it runs for both at the end therefore not repeating code.
     Line 364-366'''
     y_pred = model.predict(X_test)
     y_difference = np.subtract(y_pred, Y_test)
-    
+
     if multivariate:
         visualisation(y_pred=y_pred[:,0].reshape(-1,1),Y_test=Y_test[:,0].reshape(-1,1), y_difference=y_difference[:,0].reshape(-1,1))
         visualisation(y_pred=y_pred[:,1].reshape(-1,1),Y_test=Y_test[:,1].reshape(-1,1), y_difference=y_difference[:,1].reshape(-1,1))
     else:
         visualisation(y_pred,Y_test, y_difference)
-    
+
     #Looking into sframe as an alternative to pandas, has s3 support
     #Tensorflow also has s3 and GCP support if you install from source and enable it
     sf = SFrame(build_csv(build_data(sample)[5], build_data(sample)[7]))
     sf.explore()
     sf.show()
-    
+
 else:
 #Neural Network architecture
-    
+
     def build_regressor():
 
         #Initialising neural network
@@ -283,9 +300,9 @@ else:
         regressor.compile(optimizer='adam', loss='mse', metrics=[root_mean_squared_error])
 
         return regressor
-    
+
     output_dim = 2 if multivariate else 1 
-    
+
     #Callbacks
     from keras.callbacks import History, TensorBoard, TerminateOnNaN, ModelCheckpoint
 
@@ -294,34 +311,34 @@ else:
 
     #Model stops if loss function is nan, this happens when there is missing data
     terminate_on_nan = TerminateOnNaN()
-    
+
     #Save model
     Checkpoint = ModelCheckpoint(cp, monitor='root_mean_squared_error', verbose=verbose, save_best_only=True)
 
     #Create tensorboard
     tensorboard = TensorBoard(log_dir=path_to_tensorboard, histogram_freq=0, write_graph=True, write_images=True)     
-        
+
     def tensorboard_callback():
         callbacks=[history, tensorboard, terminate_on_nan]
         print("\n----------")
         print("Saving tensorboard to {0}".format(path_to_tensorboard))
         print("----------\n")
         return callbacks
-    
+
     def checkpoint_callback():
         callbacks=[history, terminate_on_nan, Checkpoint]
         print("\n----------")
         print("Saving model to {0}".format(cp))
         print("----------\n")
         return callbacks
-    
+
     def both_callback():
         callbacks=[history, terminate_on_nan, Checkpoint, tensorboard]
         print("\n----------")
         print("Saving model to {0}\nSaving tensorboard to {1}".format(cp,path_to_tensorboard))
         print("----------\n")
         return callbacks
-    
+
     def no_callback():
         callbacks=[history, terminate_on_nan]
         return callbacks
@@ -335,8 +352,7 @@ else:
 
     def switch_case_callbacks(x):
         return callbacks_dict[x]()
-        
-        
+
     def generate_batches(files, path_to_batch):
         counter = 0
         # This line is just to make the generator infinite, keras needs this
@@ -345,41 +361,40 @@ else:
             sample = path_to_batch + sample
             print(sample)
             counter = (counter + 1) % len(files)            
-            
+
             # This method would require calling build_data() multiple times wasting cpu
             #X_train, Y_train = build_data(sample)[3:5]
-            
+
             build = build_data(sample)
             X_train = build[3]
             Y_train = build[4]
 
             yield (X_train, Y_train)
-    
+
     # Keras's scikit-learn wrapper doesn't work with fit_generator so had to separate them
     if path_to_batch is not None:
         # Files in batch
         from os import listdir
         files = listdir(path_to_batch)
-        
+
         #shuffle file order to avoid overfitting
         import random
         random.shuffle(files)
-        
+
         #Build model
         model = build_regressor()
-        
+
         #Fit model to generated data
-        '''When dealing with large data that can not fit into memory the best approach is to add the data to the model sequentially therefore only storing one file
+        '''When dealing with large datasets that can not fit into memory the best approach is to add the data to the model sequentially therefore only storing one file
         to memory at a time. It was not possible to merge the data into one file and run the model since memory would continue to increase
         during training. classification_neural_network_Merged26.csv is 734MB but used 14.2GB in memory when put into the 'dataset' variable.
         This is because '''
         history = model.fit_generator(generate_batches(files, path_to_batch),steps_per_epoch=10, epochs=100, callbacks=switch_case_callbacks(x=True))
-        
+
     else:    
         #Build data
-        X_test, Y_test, input_dim, X_train, Y_train, test_aligned_sequence, headers, sc = build_data(sample)
-        
-        
+        X_test, Y_test, input_dim, X_train, Y_train, test_aligned_sequence, headers = build_data(sample)
+
         # According to Prof. Andrew Ng Coursera Course (Understanding mini-batch gradient descent) 
         # typically batch sizes are 64, 128, 256, 512 and 1024 (Powers of two)
 
@@ -389,10 +404,10 @@ else:
                 (len(X_train) >= 10000)       : 1024,
                 (len(X_train) < 5000)         : 10          
                 }
-        
+
         def switch_case_batch(x):
             return batch_dict.get(x)
-        
+
         #Build Model
         model = KerasRegressor(build_fn=build_regressor, epochs=100, batch_size=switch_case_batch(x=True))
 
@@ -414,21 +429,20 @@ else:
         print("\n----------\n")
         input("Press Enter to continue...")
         '''
-        
         #Have to fit data to model again after cross validation
         history = model.fit(X_train, Y_train, callbacks=switch_case_callbacks(x=True))
 
     #Prediction on test data, needed to reshape array to subtract element wise
-    
+
     y_pred = model.predict(X_test) if multivariate else model.predict(X_test).reshape(-1,1)
     y_difference = np.subtract(y_pred, Y_test)
     abs_pred = np.absolute(y_difference) 
-    
+
     if multivariate:
-        
+
         max_in_index, max_in_value = max(enumerate(abs_pred[:,1]), key=lambda p: p[1])
         max_del_index, max_del_value = max(enumerate(abs_pred[:,0]), key=lambda x: x[1])
-    
+
         accuracy = ((np.sum(abs_pred <= 0.5)/len(Y_test))*50)
         mean_accuracy = y_difference.mean()
 
@@ -441,7 +455,7 @@ else:
    
     else:
         max_index, max_value = max(enumerate(abs_pred), key=lambda p: p[1])
-    
+
         accuracy = ((np.sum(abs_pred <= 0.5)/len(Y_test))*100)
         mean_accuracy = y_difference.mean()
 
@@ -450,12 +464,12 @@ else:
         print("Average model Accuracy: {0}".format(mean_accuracy))
         print("Largest difference is {0:.2f} at position {1}\nModel predicted {2:.2f} whereas actual result was {3}".format(float(max_value), max_index, float(y_pred[max_index]), int(Y_test[max_index])))
         print("Model Performance: {0:.2f}%\nNumber of correct results (+/- 0.5): {1}/{2}".format(accuracy, np.sum(abs_pred <= 0.5), len(Y_test)))
-    
+
     print("Loading dataset and graphs in browser...")
-    sf = SFrame(build_csv(test_aligned_sequence, sc))
+    sf = SFrame(build_csv(test_aligned_sequence))
     sf.explore()
     sf.show()
-    
+
 from math import sqrt
 
 #Results variance and mean, best possible score is 1.0 for variance
@@ -465,19 +479,51 @@ rmse_value = sqrt(mean_squared_error(Y_test, y_pred))
 #Single prediction
 if user_observation:
 
-    str_observation = ','.join(user_observation)
-    import re
-    fixed_observation = re.sub('true', '1', str_observation, flags=re.IGNORECASE)
-    fixed_observation = re.sub('false', '0', fixed_observation, flags=re.IGNORECASE)
+    import os
+    if os.path.isfile(''.join(user_observation)):
+        user_observation = ''.join(user_observation)
 
-    #list comprehension didn't work
-    #user_observation = [1 if x is 'True' else x for x in user_observation]
-    #user_observation = [0 if x is 'False' else x for x in user_observation]
+        data = pd.read_csv(user_observation)
 
-    list_observation = [float(i) for i in fixed_observation.split(',')]
-    new_prediction = model.predict(sc.transform(np.array([list_observation])))
-    print("\n----------\n")
-    print ("Prediction is %s" % new_prediction)
+        #headers = list(data)
+        del data['ins/dels']
+
+        #Encoding NHEJ
+        data['NHEJ'] = labelencoder.fit_transform(data['NHEJ'])
+
+        #Encoding UNMODIFIED
+        data['UNMODIFIED'] = labelencoder.fit_transform(data['UNMODIFIED'])
+
+        #Encoding frameshift
+        data['frameshift'] = labelencoder.fit_transform(data['frameshift'])
+
+        #Encoding HDR
+        data['HDR'] = labelencoder.fit_transform(data['HDR'])
+
+        data = data.iloc[:,:].values
+        test_aligned_sequence, data = data[:,0:1],data[:,1:input_dim + 1]
+
+        new_prediction = model.predict(sc.transform(data)).reshape(-1,1)
+        pred_set = np.concatenate((test_aligned_sequence,data,new_prediction),axis=1)
+
+        frame = pd.DataFrame(pred_set)
+        frame.to_csv('{0}_batch_prediction.csv'.format(date), header=False, index=False)
+        print("Predictions saved to file '{0}_batch_prediction.csv'".format(date))
+
+    else:
+        str_observation = ','.join(user_observation)
+        import re
+        fixed_observation = re.sub('true', '1', str_observation, flags=re.IGNORECASE)
+        fixed_observation = re.sub('false', '0', fixed_observation, flags=re.IGNORECASE)
+
+        #list comprehension didn't work
+        #user_observation = [1 if x is 'True' else x for x in user_observation]
+        #user_observation = [0 if x is 'False' else x for x in user_observation]
+
+        list_observation = [float(i) for i in fixed_observation.split(',')]
+        new_prediction = model.predict(sc.transform(np.array([list_observation])))
+        print("\n----------\n")
+        print ("Prediction is %s" % new_prediction)
 
 #Opens tensorboard in browser if user saves new tensorboard
 import subprocess
@@ -500,6 +546,3 @@ if cp:
         default = '{0}/machine-learning/predictions/{1}_miseq_predictions'.format(home, date)
         file_name = input('Type path and file name [Press enter to keep default: {0}]:'.format(default))
         frame.to_csv(file_name or default ,index=False)
-
-
-
